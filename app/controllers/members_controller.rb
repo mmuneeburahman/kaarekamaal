43cb5f25ee141ead7a91156042e7782e95676fe9
Muhammad Muneeb Ur Rahman <{ID}+{username}@users.noreply.github.com>
2023-01-04 02:22:15 +0500

    add new.html.erb for member controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,10 @@
 class MembersController < ApplicationController
+    before_action :authenticate_user!, only: [:new]
+
+    def new
+        @user = User.find(current_user.id)
+    end
+
+    def create
+    end
 end
