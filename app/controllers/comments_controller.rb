f61b50edea326a8bcde6f460ca01c912d8364407
Muhammad Muneeb Ur Rahman <{ID}+{username}@users.noreply.github.com>
2023-01-04 20:40:12 +0500

    Delete comment added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,15 @@ class CommentsController < ApplicationController
         # binding.break
         @comment = @case.comments.create(comment_params)
         redirect_to case_path(@case)
-      end
+    end
     
+    def destroy
+        @case = Case.find(params[:case_id])
+        @comment = @case.comments.find(params[:id])
+        @comment.destroy
+        redirect_to case_path(@case), status: :see_other
+    end
+
     private
     def comment_params
         params.require(:comment).permit(:commenter, :body, :user_id)
